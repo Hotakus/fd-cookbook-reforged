92736a10dbc04579c0fb4e0bce4ae6a4e48e32f4
hotakus <ttowfive@gmail.com>
2023-12-08 20:53:29 +0800

    chore(all): mass changes of project structure

---
+++ Forge/src/main/java/net/hotakus/fdcookbook/FDCookBook.java
@@ -0,0 +1,64 @@
+package net.hotakus.fdcookbook;
+
+import com.mojang.logging.LogUtils;
+import net.hotakus.fdcookbook.items.ItemsRegister;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import vazkii.patchouli.api.PatchouliAPI;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(FDCookBook.MOD_ID)
+public class FDCookBook {
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "fdcookbook";
+
+    public FDCookBook() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        //ToysCraftRegister.register(eventBus);
+        ItemsRegister.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // Some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        // LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // Register a new block here
+//            LOGGER.info("HELLO from Register Block");
+        }
+    }
+}
