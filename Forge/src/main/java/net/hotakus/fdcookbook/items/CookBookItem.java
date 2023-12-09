f3a10b6092ce56c6d98a9d1a3ab7e9acfc0afb1d
hotakus <ttowfive@gmail.com>
2023-12-09 20:29:07 +0800

    refactor: can be custom item now

--- Forge/src/main/java/net/hotakus/fdcookbook/items/CookBookItem.java
+++ Forge/src/main/java/net/hotakus/fdcookbook/items/CookBookItem.java
@@ -1,47 +1,111 @@
 package net.hotakus.fdcookbook.items;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import vazkii.patchouli.api.PatchouliAPI;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public class CookBookItem extends Item {
+
+    ResourceLocation bookRL = Registry.ITEM.getKey(this);
+
     public CookBookItem() {
         super(new Properties()
-                .tab(CreativeModeTab.TAB_MISC)
+                .tab(CreativeModeTab.TAB_FOOD)
                 .stacksTo(1)
                 .rarity(Rarity.EPIC)
         );
     }
 
-    @NotNull
     @Override
-    public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
-        ItemStack stack = playerIn.getItemInHand(handIn);
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+        InteractionResult res = InteractionResult.FAIL;
 
-        if (playerIn instanceof ServerPlayer player) {
-            //UseItemSuccessTrigger.INSTANCE.trigger(player, stack, player.serverLevel(), player.getX(), player.getY()
-                    //, player.getZ());
-            //PatchouliAPI.get().openBookGUI(player, BuiltInRegistries.ITEM.getKey(this));
-            //playerIn.playSound(BotaniaSounds.lexiconOpen, 1F, (float) (0.7 + Math.random() * 0.4));
+        if (pUsedHand == InteractionHand.MAIN_HAND && !Screen.hasShiftDown()) {
+            if (pPlayer instanceof ServerPlayer) {
+                PatchouliAPI.get().openBookGUI((ServerPlayer) pPlayer, bookRL);
+                res = InteractionResult.SUCCESS;
+            }
+        } else if (Screen.hasShiftDown()) {
+            System.out.println();
+        } else {
+            res = InteractionResult.PASS;
         }
 
-        return InteractionResultHolder.sidedSuccess(stack, worldIn.isClientSide());
+        return new InteractionResultHolder<>(res, stack);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+
+        InteractionResult res = InteractionResult.PASS;
+
+        if (Screen.hasShiftDown()) {
+            BlockPos pos = pContext.getClickedPos();
+            Level level = pContext.getLevel();
+
+            BlockState blockState = level.getBlockState(pos);
+
+            System.out.println(blockState.getBlock().getRegistryName().getPath());
+            pContext.getPlayer().sendMessage(new TextComponent(blockState.getBlock().getRegistryName().getPath()),
+                    pContext.getPlayer().getUUID());
+
+            pContext.getPlayer().swing(pContext.getHand(), true);
+
+            res = InteractionResult.SUCCESS;
+        }
+
+        // TODO: Mapping items to Entries
+        // PatchouliAPI.get().openBookEntry(bookRL,);
+
+        return res;
+    }
+
+    public static Component getEdition() {
+        try {
+            return PatchouliAPI.get().getSubtitle(new ResourceLocation("fdcookbook:fd_cookbook"));
+        } catch (IllegalArgumentException e) {
+            return new TextComponent(""); // TODO Adjust Patchouli because first search tree creation is too early to get the edition
+        }
+    }
+
+    public static Component getTitle(ItemStack stack) {
+        Component title = stack.getHoverName();
+
+        String akashicTomeNBT = "akashictome:displayName";
+        if (stack.hasTag() && stack.getTag().contains(akashicTomeNBT)) {
+            title = Component.Serializer.fromJson(stack.getTag().getString(akashicTomeNBT));
+        }
+
+        return title;
     }
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
-        pTooltipComponents.add(new TranslatableComponent("tooltip.fdcookbook.fd_cookbook"));
+        pTooltipComponents.add(getEdition().copy().withStyle(ChatFormatting.GRAY));
+        if (!Screen.hasShiftDown()) {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.fdcookbook.shift"));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.fdcookbook.fd_cookbook.tooltip.normal"));
+        }
     }
 }
--- Forge/src/main/java/net/hotakus/fdcookbook/items/ItemsRegister.java
+++ Forge/src/main/java/net/hotakus/fdcookbook/items/ItemsRegister.java
@@ -2,6 +2,7 @@ package net.hotakus.fdcookbook.items;
 
 import net.hotakus.fdcookbook.FDCookBook;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,8 +12,9 @@ public class ItemsRegister {
     private static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FDCookBook.MOD_ID);
 
-//    public static final RegistryObject<Item> FD_COOKBOOK =
-//            ITEMS.register("fd_cookbook", CookBookItem::new);
+    public static final RegistryObject<Item> FD_COOKBOOK =
+            ITEMS.register("fd_cookbook", CookBookItem::new);
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
